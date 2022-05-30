22f2ad3a5f227108cb4c0b557a577028a7f9b10d
MashaChe1 <103838851+MashaChe1@users.noreply.github.com>
2022-05-30 17:29:22 +0300

    first commit

---
+++ my-app/src/CComponent.js
@@ -0,0 +1,10 @@
+import React, { CComponent } from 'react';
+
+export default class CComponent extends CComponent {
+   render () {
+      return (
+         <div>Hello</div>
+      )
+      
+   }
+}
---
+++ my-app/src/FComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FComponent () {
+   const [ age, setage ] = useState (0);
+   
+   return (
+      <div>
+         <h1>Hello again!</h1>
+      </div>
+   )
+}
