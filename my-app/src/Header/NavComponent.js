752f6ac5aeab5f24ec2e2d0ad2a3347b309d5ebd
MashaChe1 <103838851+MashaChe1@users.noreply.github.com>
2022-06-03 17:59:47 +0300

    Commit in class 2

--- my-app/src/Header/NavComponent.js
+++ my-app/src/Header/NavComponent.js
@@ -2,35 +2,34 @@ import React from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import cities from './cities.json';
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+
 
 export default function NavComponent (){
 
    const [ selectedCity, setSelectedCity ] = useState({city: "Choose city"});
+   const [units, setUnits] = useState('');
 
    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
          <Container>
-            <Navbar.Brand href="#home">Weather</Navbar.Brand>
+            <Navbar.Brand href="#home">Weather {units} </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                <Navbar.Collapse id="responsive-navbar-nav">
                    <Nav className="me-auto">
+                   <Nav.Link onClick={() => setUnits('°C')}>°C</Nav.Link>
+                     <Nav.Link onClick={() => setUnits('°F')} >°F</Nav.Link>
                      <NavDropdown title= {selectedCity.city} id="collasible-nav-dropdown">
 
                         {cities.map ( city => 
                         <NavDropdown.Item onClick={() => setSelectedCity(city)} href="#action/3.1">{city.city}</NavDropdown.Item>
                          )}
-
-                     </NavDropdown>
+                     </NavDropdown> 
                   </Nav>
                </Navbar.Collapse>
-            <div>
-               <Button variant="outline-secondary">°C</Button> {'  '}
-               <Button variant="outline-secondary">°F</Button>
-            </div>
          </Container>
       </Navbar>
    )
 }
+            
 
 
