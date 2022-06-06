dc947fec46a26e0340a61e50e6dd73093e077661
MashaChe1 <103838851+MashaChe1@users.noreply.github.com>
2022-06-06 18:06:29 +0300

    homework3

--- my-app/src/Header/FormComponent.js
+++ my-app/src/Header/FormComponent.js
@@ -1,20 +1,42 @@
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, FormLabel } from 'react-bootstrap';
 import cities from './cities.json';
+import dataTypes from './type.json'
 
 export default function FormComponent (props){
 
    return (
       <Container>
          <Form onSubmit={props.handleOnSubmitForm}>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-               <Form.Label>Choose city</Form.Label>
+         <Form.Group className="mb-3" controlId="dataType"> 
+         <FormLabel>Choose data type</FormLabel>
+         {dataTypes.map ( dtype => 
+                           <Form.Check
+                           key={dtype.value}
+                           type="checkbox"
+                           name="dataType"
+                           label={dtype.label}
+                           value={dtype.value}
+                         />
+                           )}
+         </Form.Group>  
+         <Form.Group className="mb-3" controlId="language">  
+         <FormLabel>TChoose language</FormLabel>
                <Form.Select name="city" aria-label="Default select example">
-                  <option></option>
+                  <option>Choose language</option>
+                  {['en',['fi','ru']].map ( language => 
+                           <option key={language}>{language}</option>
+                           )}
+               </Form.Select>
+         </Form.Group>
+            <Form.Group className="mb-3" controlId="city">
+               <Form.Label></Form.Label>
+               <Form.Select name="city" aria-label="Default select example">
+                  <option>Choose city</option>
                   {cities.map ( city => 
                            <option key={city.city}>{city.city}</option>
                            )}
-               </Form.Select>
-            </Form.Group>
+            </Form.Select>
+         </Form.Group>
             <Form.Group controlId="unit">
                <Form.Label>Choose unit</Form.Label>
                   <Form.Check
