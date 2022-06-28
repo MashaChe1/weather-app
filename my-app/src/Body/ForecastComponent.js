2644eba712f468c8bf6467d026b4cfe407bd6ebc
MashaChe1 <103838851+MashaChe1@users.noreply.github.com>
2022-06-28 19:03:08 +0300

    lesson 12 - 1

--- my-app/src/Body/ForecastComponent.js
+++ my-app/src/Body/ForecastComponent.js
@@ -19,8 +19,9 @@ export default function ForecastComponent(props) {
 
         for (const [index, data] of response.list.entries()) {
 
-          const dayOfDate = moment.unix(data.dt).date();
-          const prevDayOfDate = oneDay.length ? moment.unix(oneDay[oneDay.length - 1].dt).date() : dayOfDate;
+          data.dt = data.dt + response.city.timezone;
+          const dayOfDate = moment.unix(data.dt).utc().date();
+          const prevDayOfDate = oneDay.length ? moment.unix(oneDay[oneDay.length - 1].dt).utc().date() : dayOfDate;
 
           if (dayOfDate === prevDayOfDate) {
             oneDay.push(data);
@@ -53,10 +54,10 @@ export default function ForecastComponent(props) {
     <>
       <Tabs className="mb-3 mt-2">
         {days.map((day, index1) => (
-          <Tab eventKey={index1} key={index1} title={'Day ' + moment.unix(day[0].dt).date()}>
+          <Tab eventKey={index1} key={index1} title={'Day ' + moment.unix(day[0].dt).utc().date()}>
             <Tabs className="mb-3 mt-2">
               {day.map((data, index2) => (
-                <Tab eventKey={index2} key={index2} title={moment.unix(data.dt).format('HH:mm')}>
+                <Tab eventKey={index2} key={index2} title={moment.unix(data.dt).utc().format('HH:mm')}>
                   <DataComponent {...props} weather={data} />
                 </Tab>
               ))}
